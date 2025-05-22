cdf3d882c01584156f5f97fd8a7977a95938f3fd
livia <liviamajzoubsilva19@gmail.com>
2025-05-22 16:42:24 -0300

    Update prova 2 - lista de exercicios ref..c

--- prova 2 - lista de exercicios ref..c
+++ prova 2 - lista de exercicios ref..c
@@ -1,25 +1,30 @@
 #include <stdio.h>
 #include <math.h>
 
-int calcula_raizes(float a, float b, float c, float *x1, float *x2){
+int calcula_raizes(float a, float b, float c, float *x1, float *x2)
+{
 
     float delta = b*b - 4*a*c;
 
-    if (delta < 0){
+    if (delta < 0)
+    {
         return 0;
-}
-    else if (delta == 0){
+    }
+    else if (delta == 0)
+    {
         *x1 = *x2 = -b / (2*a);
         return 1;
     }
-    else{
+    else
+    {
         *x1 = ( -b + sqrt(delta)) / (2*a);
         *x2 = ( -b - sqrt(delta)) / (2*a);
         return 2;
     }
 }
 
-int main(){
+int main()
+{
 
     float a, b, c, x1, x2;
 
@@ -37,13 +42,19 @@ int main(){
     resultado = calcula_raizes(a, b, c, &x1, &x2);
 
     if (resultado == 2)
-    {printf("Duas raizes reais: x1 = %.2f, x2 = %.2f\n", x1, x2);}
+    {
+        printf("Duas raizes reais: x1 = %.2f, x2 = %.2f\n", x1, x2);
+    }
 
     else if (resultado == 1)
-    {printf("Uma raiz real: x = %.2f\n", x1);}
+    {
+        printf("Uma raiz real: x = %.2f\n", x1);
+    }
 
     else
-    {printf("Não existem raizes reais.\n");}
+    {
+        printf("NÃ£o existem raizes reais.\n");
+    }
 
     return 0;
 }
